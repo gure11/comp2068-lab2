0e3f890da195bfe353de8bdf099b7e82c8398a6d
Gurbinder Singh <200393494@student.georgianc.on.ca>
2020-06-08 23:27:55 -0400

    lab2 done

---
+++ lab2/lab2/routes/index.js
@@ -0,0 +1,36 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    // requesting query method
+    req.query.method;
+    req.query.x, req.query.y;
+    if (req.query.method == "add") {
+        console.log(req.query.x, '+', req.query.y, '=', parseInt(req.query.x) + parseInt(req.query.y));
+    }
+    // requesting query x and y variables and subtracting
+    else if (req.query.method == "subtract") {
+        console.log(req.query.x, '-', req.query.y, '=', req.query.x - req.query.y);
+    }
+         // requesting query x and y variables and multiply
+    else if (req.query.method == "multiply") {
+        console.log(req.query.x, '*', req.query.y, '=', req.query.x * req.query.y);
+    }
+          // requesting query x and y variables and divide
+    else if (req.query.method == "divide") {
+        console.log(req.query.x, '/', req.query.y, '=', req.query.x / req.query.y);
+    }
+    else {
+        console.log("please enter valid method")
+    }
+
+
+
+
+
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
